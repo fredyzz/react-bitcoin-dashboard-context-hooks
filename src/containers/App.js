7fba7c43a25cf3031bbe52bca8092f509afff897
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-06-28 17:14:57 -0300

    React app created and folder structure ready

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/containers/App.css'
+
+function App() {
+	return (
+		<div className="App">
+			<h1>React Toast</h1>
+		</div>
+	)
+}
+
+export default App
