bfc75a154e825d7f7567aca6cd150d585e05bfaa
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-07-06 22:14:29 -0300

    Updates

---
+++ precache-manifest.89e2203a20ac325d4374a4de1251f44d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a75a62fd4cdb8f8fb63c3a990f00e1",
+    "url": "/react-bitcoin-dashboard-context-hooks/index.html"
+  },
+  {
+    "revision": "9303a02318af0020fbb7",
+    "url": "/react-bitcoin-dashboard-context-hooks/static/css/main.7c5b2be1.chunk.css"
+  },
+  {
+    "revision": "36d9f5dd7c6bd798090b",
+    "url": "/react-bitcoin-dashboard-context-hooks/static/js/2.9511cdfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bitcoin-dashboard-context-hooks/static/js/2.9511cdfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9303a02318af0020fbb7",
+    "url": "/react-bitcoin-dashboard-context-hooks/static/js/main.2449271f.chunk.js"
+  },
+  {
+    "revision": "f29852b8ee7b63828bd1",
+    "url": "/react-bitcoin-dashboard-context-hooks/static/js/runtime-main.f54eaf14.js"
+  }
+]);
